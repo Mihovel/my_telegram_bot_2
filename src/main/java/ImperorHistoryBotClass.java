b0132ce8c8f15a6a11bb639c26ac78635f788d4f
John Doe <johndoe@example.com>
2019-11-04 22:55:04 +0300

    added proxy

--- src/main/java/ImperorHistoryBotClass.java
+++ src/main/java/ImperorHistoryBotClass.java
@@ -1,10 +1,19 @@
+import net.sourceforge.jwbf.core.contentRep.Article;
+import net.sourceforge.jwbf.mediawiki.bots.MediaWikiBot;
+import org.telegram.abilitybots.api.bot.AbilityBot;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
-public class ImperorHistoryBotClass extends TelegramLongPollingBot {
+public class ImperorHistoryBotClass extends AbilityBot {
+    @Override
+    public int creatorId() {
+        return 0;
+    }
 
-    public void onUpdateReceived(Update update) {
-
+    protected ImperorHistoryBotClass(String botToken, String botUsername, DefaultBotOptions options) {
+        super(botToken, botUsername, options);
     }
 
     public String getBotUsername() {
@@ -13,5 +22,37 @@ public class ImperorHistoryBotClass extends TelegramLongPollingBot {
 
     public String getBotToken() {
         return "1005113234:AAHg9rfkOYfiftC_sOmISYX5vWpk7Xen7Pg";
+    }
+    /**
+     * Метод для приема сообщений.
+     * @param update Содержит сообщение от пользователя.
+     */
+    @Override
+    public void onUpdateReceived(Update update) {
+        String message = update.getMessage().getText();
+
+        MediaWikiBot wikiBot = new MediaWikiBot("https://en.wikipedia.org/w/");
+        Article article = wikiBot.getArticle("42");
+        System.out.println(article.getText().substring(5, 42));
+
+
+        sendMsg(update.getMessage().getChatId().toString(), article.getText().substring(5, 42));
+    }
+
+    /**
+     * Метод для настройки сообщения и его отправки.
+     * @param chatId id чата
+     * @param message Строка, которую необходимот отправить в качестве сообщения.
+     */
+    public synchronized void sendMsg(String chatId, String message) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(message);
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+            System.out.println(e.toString());
+        }
     }
 }
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,22 +1,39 @@
+import org.apache.http.HttpHost;
+import org.apache.http.client.config.RequestConfig;
+import org.telegram.telegrambots.ApiContext;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class MainClass {
+    private static String BOT_NAME = "My test bot";
+    private static String BOT_TOKEN = "1005113234:AAHg9rfkOYfiftC_sOmISYX5vWpk7Xen7Pg" /* your bot's token here */;
+
+    private static String PROXY_HOST = "51.158.108.135" /* proxy host */;
+    private static Integer PROXY_PORT = 8811 /* proxy port */;
 
     public static void main(String[] args) {
-        System.setProperty("java.net.useSystemProxies", "true");
-        System.setProperty("http.proxyHost", "http://proxy.memorynotfound.com");
-        System.setProperty("http.proxyPort", "80");
-        System.setProperty("http.nonProxyHosts", "localhost|127.0.0.1");
-
-// HTTPS
-        System.setProperty("https.proxyHost", "https://proxy.memorynotfound.com");
-        System.setProperty("https.proxyPort", "443");
-        ApiContextInitializer.init();
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
-            telegramBotsApi.registerBot(new ImperorHistoryBotClass());
+            ApiContextInitializer.init();
+
+            // Create the TelegramBotsApi object to register your bots
+            TelegramBotsApi botsApi = new TelegramBotsApi();
+
+            // Set up Http proxy
+            DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
+
+            HttpHost httpHost = new HttpHost(PROXY_HOST, PROXY_PORT);
+
+            RequestConfig requestConfig = RequestConfig.custom().setProxy(httpHost).setAuthenticationEnabled(false).build();
+            botOptions.setRequestConfig(requestConfig);
+            botOptions.setHttpProxy(httpHost);
+
+            // Register your newly created AbilityBot
+            ImperorHistoryBotClass bot = new ImperorHistoryBotClass(BOT_TOKEN, BOT_NAME, botOptions);
+
+            botsApi.registerBot(bot);
+
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
